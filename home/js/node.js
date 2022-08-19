83129598ef1c2af09e0a49928189fd445657a697
azmu0520 <azizbekbootcam@gmail.com>
2022-08-19 11:04:48 +0500

    done

--- home/js/node.js
+++ home/js/node.js
@@ -3,10 +3,3 @@ var menu = document.querySelector('.menu');
 btn.addEventListener('click', () => {
   menu.classList.toggle('open');
 });
-
-var scroller = document.querySelector('#body');
-var gift__box = document.querySelector('.gift__box');
-
-document.body.addEventListener('click', () => {
-  console.log(document.body.scrollTop);
-});
