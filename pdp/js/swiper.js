15fb901796782428ec565226b33a544bd111648d
azmu0520 <azizbekbootcam@gmail.com>
2022-08-22 14:10:24 +0500

    done

--- pdp/js/swiper.js
+++ pdp/js/swiper.js
@@ -4,3 +4,23 @@ const swiper = new Swiper('.swiper1', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const swiper2 = new Swiper('.swiper2', {
+  navigation: {
+    nextEl: '.swiper-button-next-2',
+    prevEl: '.swiper-button-prev-2',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  slidesPerView: 2,
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+    },
+  },
+});
